b263448a63ab0e361ca5d277173c238607cf9499
Jens <34352934+jensmunkerud@users.noreply.github.com>
2024-11-09 09:33:18 +0100

    Last version with I2C

--- src/main.cpp
+++ src/main.cpp
@@ -2,30 +2,52 @@
 #include <ArduinoBLE.h>
 #include <Wire.h>
 #include <secrets.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
+#include <Adafruit_I2CDevice.h>
+// #include <Adafruit_GFX.h>
+// #include <Adafruit_SSD1306.h>
 // #include <Servo.h>
-Adafruit_SSD1306 display(128, 64, &Wire, -1);
-#define I2CSlave 9
+// Adafruit_SSD1306 display(128, 64, &Wire, -1);
+int slaveID1 = 1;
+int slaveID2 = 2;
+#define I2C_ADDRESS 0x60
+Adafruit_I2CDevice i2c_dev = Adafruit_I2CDevice(I2C_ADDRESS);
 
 // PARAMETERS
 unsigned long currentMillis = 0;
 unsigned long previousMillis = 0;
+unsigned long lastBLEPackage = 0;
 int16_t xData = 0;
 int16_t yData = 0;
-String rawData;
+int8_t exInner = 0;			// 1 extend, 0 nothing, -1 retract
+int8_t exOuter = 0;			// 1 extend, 0 nothing, -1 retract
+int8_t smooth = 1;			// 0 instant, 1 default smoothness
+int8_t switcher = 0;		// 0 both, 1 lower, 2 upper
+int RPMCounts[4];
+bool dataReceived;
+int talkingSlaves[2] = {slaveID1, slaveID2};
 
 // Sets bluetooth characteristics
 const char * serviceUUID = sUUID;
 const char * xDataUUID = xUUID;
 const char * yDataUUID = yUUID;
+const char * exInnerUUID = extendInnerUUID;
+const char * exOuterUUID = extendOuterUUID;
+const char * smoothUUID = smothUUID;
+const char * stageUUID = stagUUID;
 BLEService joystickService(serviceUUID);
-BLEIntCharacteristic xCharacteristic(xDataUUID, BLEWriteWithoutResponse); // String characteristic for X, Y data
-BLEIntCharacteristic yCharacteristic(yDataUUID, BLEWriteWithoutResponse); // String characteristic for X, Y data
+BLEIntCharacteristic xCharacteristic(xDataUUID, BLEWriteWithoutResponse);
+BLEIntCharacteristic yCharacteristic(yDataUUID, BLEWriteWithoutResponse);
+BLEIntCharacteristic exInnerCharacteristic(exInnerUUID, BLEWriteWithoutResponse);
+BLEIntCharacteristic exOuterCharacteristic(exOuterUUID, BLEWriteWithoutResponse);
+BLEIntCharacteristic smoothCharacteristic(smoothUUID, BLEWrite);
+BLEIntCharacteristic stageCharacteristic(stageUUID, BLEWrite);
 
-struct XY {
+struct I2CData {
 	int x;
 	int y;
+	int smooth;
+	int exOuter;
+	int exInner;
 };
 
 // PROGRAM START
@@ -35,39 +57,89 @@ void advertiseBLE() {
 	BLE.setAdvertisedService(joystickService);
 	joystickService.addCharacteristic(xCharacteristic);
 	joystickService.addCharacteristic(yCharacteristic);
+	joystickService.addCharacteristic(exInnerCharacteristic);
+	joystickService.addCharacteristic(exOuterCharacteristic);
+	joystickService.addCharacteristic(smoothCharacteristic);
+	joystickService.addCharacteristic(stageCharacteristic);
 	BLE.addService(joystickService);
 	BLE.advertise();
 	Serial.println("BLE is advertising...");
 }
 
-void startScreen() {
-	display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-	display.clearDisplay();
-	display.display();
-	display.setTextColor(1);
-	display.setTextSize(2);
+// void startScreen() {
+// 	display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+// 	display.clearDisplay();
+// 	display.display();
+// 	display.setTextColor(1);
+// 	display.setTextSize(2);
+// }
+
+// void updateScreen() {
+// 	display.clearDisplay();
+// 	display.setCursor(0, 0);
+// 	// display.print("x = ");
+// 	// display.print(xData);
+// 	// display.setCursor(0, 20);
+// 	// display.print("y = ");
+// 	// display.print(yData);
+// 	display.print("mot1: ");
+// 	display.print(RPMCounts[0]);
+// 	display.setCursor(0, 20);
+// 	display.print("mot2: ");
+// 	display.print(RPMCounts[1]);
+// 	display.display();
+// }
+
+// Transmit XY data to I2C device
+void sendSlaveInstance(I2CData I2CData, int slave) {
+	Wire.beginTransmission(slave);
+	String data = 
+	String(I2CData.x)+","+
+	String(I2CData.y)+","+
+	String(I2CData.smooth)+","+
+	String(I2CData.exInner)+","+
+	String(I2CData.exOuter);
+	Wire.write(data.c_str());
+	// unsigned long start = millis();
+	// while (Wire.endTransmission() != 0) {  // Check if transmission is successful
+	// 	if (millis() - start > 10000) {  // Timeout after 100 ms
+	// 		Serial.print("I2C timeout on slave "); Serial.println(slave);
+	// 		break;
+	// 	}
+	// }
+	Wire.endTransmission();
+	Serial.print("Wrote to slave "); Serial.println(slave);
+	// Serial.print("Sent x: "); Serial.print(int(I2CData.x)); Serial.print("    y: "); Serial.println(int(I2CData.y));
 }
 
-void updateScreen() {
-	display.clearDisplay();
-	display.setCursor(0, 0);
-	display.print("x = ");
-	display.print(xData);
-	display.setCursor(0, 20);
-	display.print("y = ");
-	display.print(yData);
-	display.setCursor(0, 40);
-	display.print(rawData);
-	display.display();
+// Switches between upper, lower or both stages
+void sendSlave(I2CData I2CData, int switchLoc) {
+	// switcher 0 both, 1 upper, -1 lower
+	if (switchLoc == 1) {
+		sendSlaveInstance(I2CData, slaveID1);
+	}
+	if (switchLoc == 2) {
+		sendSlaveInstance(I2CData, slaveID2);
+	} else {
+		sendSlaveInstance(I2CData, slaveID1);
+		sendSlaveInstance(I2CData, slaveID2);
+	}
 }
+
 
 void setup() {
 	pinMode(LED_BUILTIN, OUTPUT);
-
-	// Starts I2C as MASTER
-	Wire.begin(); 
+	Wire.begin();					// Starts I2C as MASTER
+	Wire.setTimeout(3000);
+	if (!i2c_dev.begin()) {
+		Serial.print("Did not find device at 0x");
+		Serial.println(i2c_dev.address(), HEX);
+		while (1);
+	}
 	Serial.begin(9600);
-	startScreen();
+	// startScreen();					// Starts screen
+	// updateScreen();					// Initilializes screen
+	// sendSlave({0, 0, 1, 0, 0}, 0);		// Stops all motors // THIS BUGS OUT ON STARTUP
 	// Starts Bluetooth
 	if (!BLE.begin()) {
 		Serial.println("starting BLE failed!");
@@ -76,23 +148,43 @@ void setup() {
 	advertiseBLE();
 }
 
-// Transmit XY data to I2C device
-void sendSlave(XY XYdata) {
-	Wire.beginTransmission(I2CSlave);
-	String data = String(XYdata.x) + "," + String(XYdata.y);
-	Wire.write(data.c_str());
-	Wire.endTransmission();
-	Serial.print("Sent x: "); Serial.print(int(XYdata.x)); Serial.print("    y: "); Serial.println(int(XYdata.y));
-}
 
 // Read the XY data sent by the joystick, and forwarding to slaves
 void receiveBLEData() {
+	dataReceived = false;
 	if (xCharacteristic.written()) {
 		xCharacteristic.readValue((int16_t*)&xData, sizeof(int16_t));
+		dataReceived = true;
 	}
 	if (yCharacteristic.written()) {
 		yCharacteristic.readValue((int16_t*)&yData, sizeof(int16_t));
-		sendSlave({xData, yData});
+		dataReceived = true;
+	}
+	if (exOuterCharacteristic.written()) {
+		exOuterCharacteristic.readValue((int8_t*)&exOuter, sizeof(int8_t));
+		// Serial.print("Received extend OUTER: "); Serial.println(exOuter);
+		dataReceived = true;
+	}
+	if (exInnerCharacteristic.written()) {
+		exInnerCharacteristic.readValue((int8_t*)&exInner, sizeof(int8_t));
+		// Serial.print("Received extend INNER: "); Serial.println(exInner);
+		dataReceived = true;
+	}
+	if (smoothCharacteristic.written()) {
+		// Smoothness control, 1 = smooth, 0 = instant
+		smoothCharacteristic.readValue((int8_t*)&smooth, sizeof(int8_t));
+		// Serial.print("Received SMOOTH: "); Serial.println(exOuter);
+		dataReceived = true;
+	}
+	if (stageCharacteristic.written()) {
+		stageCharacteristic.readValue((int8_t*)&switcher, sizeof(int8_t));
+		// Serial.print("Received STAGE: "); Serial.println(switcher);
+		dataReceived = true;
+	}
+	if (dataReceived) {
+		I2CData dat = {xData, yData, smooth, exOuter, exInner};
+		sendSlave(dat, switcher);
+		lastBLEPackage = millis();
 	}
 }
 
@@ -102,27 +194,35 @@ void loop() {
 	if (central) {
 		Serial.println("Connected to central device");
 		digitalWrite(LED_BUILTIN, HIGH);
+		lastBLEPackage = millis();
 		while (central.connected()) {
 			// While the phone is connected
 			receiveBLEData();
 
 			// Happens once a second while connected
 			unsigned long currentMillis = millis();
-			if (currentMillis - previousMillis >= 100) {
-				previousMillis = currentMillis;
-				Wire.requestFrom(I2CSlave, 20);
-				while (Wire.available()) {
-					char c = Wire.read();               // Read each byte from the slave
-					rawData += c;
-				}
-				updateScreen();
-				rawData = "";
+			// if (currentMillis - previousMillis >= 100) {
+			// 	previousMillis = currentMillis;
+			// 	Wire.requestFrom(slaveID1, 4);
+			// 	int i = 0;
+			// 	while (Wire.available() && i < 4) {
+			// 		RPMCounts[i] = (int8_t)Wire.read();
+			// 		i++;
+			// 	}
+			// 	updateScreen();
+			// }
+			if (currentMillis - lastBLEPackage > 10000) {
+				Serial.println("Bluetooth timed out");
+				BLE.disconnect();
+				if (!central.connected()) break;  // Confirm disconnection
 			}
 		}
 	Serial.println("Disconnected from central device");
 	digitalWrite(LED_BUILTIN, LOW);
 	advertiseBLE();
-	sendSlave({0, 0});
+	sendSlave({0, 0, 1, 0, 0}, 0);		// Stops all motors
+	xData = 0; yData = 0;
+	// updateScreen();
 	}
 	// Blinks LED_BUILTIN when we are searching for bluetooth devices
 	unsigned long currentMillis = millis();
--- src/slave.h
+++ src/slave.h
@@ -1,8 +1,8 @@
 #include <Wire.h>
 
 // Sets pins
-#define mot1		3
-#define mot1Dir		2
+#define mot1		3	// Blue
+#define mot1Dir		2	// White
 #define mot2		5
 #define mot2Dir		4
 #define mot3		6
@@ -10,38 +10,37 @@
 #define mot4		9
 #define mot4Dir		8
 
-#define pistonInner	10
-#define pistonOuter	11
+// REDEFINE THESE
+#define pistonInnerExt	10
+#define pistonInnerRetr	11
+#define pistonOuterExt	12
+#define pistonOuterRetr	13
 
-// #define MOTOR1_FG_PIN A0
-// #define MOTOR2_FG_PIN A1
-// #define MOTOR3_FG_PIN A2
-// #define MOTOR4_FG_PIN A3
+#define mot1RPM A0	// Yellow
+#define mot2RPM A1
+#define mot3RPM A2
+#define mot4RPM A3
 
-#define I2C_ID		9
+#define slaveID		1
 
-// // Variables to store pulse counts and last state for each motor
-// unsigned long pulseCount1 = 0;
-// unsigned long pulseCount2 = 0;
-// unsigned long pulseCount3 = 0;
-// unsigned long pulseCount4 = 0;
-
-// int lastState1 = LOW;
-// int lastState2 = LOW;
-// int lastState3 = LOW;
-// int lastState4 = LOW;
-
-// unsigned long lastMillis = 0; // Last time RPM was calculated
-// const unsigned long rpmCalcInterval = 1000; // Interval to calculate RPM (in ms)
-
-// // RPM results
-// float rpm1 = 0;
-// float rpm2 = 0;
-// float rpm3 = 0;
-// float rpm4 = 0;
+// Variables to store pulse counts and last state for each motor
+unsigned long pulseCount1 = 0;
+unsigned long pulseCount2 = 0;
+unsigned long pulseCount3 = 0;
+unsigned long pulseCount4 = 0;
+int lastState1 = LOW;
+int lastState2 = LOW;
+int lastState3 = LOW;
+int lastState4 = LOW;
+unsigned long lastMillis = 0; // Last time RPM was calculated
+const unsigned long rpmCalcInterval = 1000; // Interval to calculate RPM (in ms)
+float rpm1 = 0;
+float rpm2 = 0;
+float rpm3 = 0;
+float rpm4 = 0;
 
 // PARAMETERS
-float smoothness = 0;		// units / sec		(disable = 0)
+float smoothness = 255;		// units / sec		(disable = 0)
 float currentX = 0;
 float currentY = 0;
 bool hasUpdated;
@@ -49,98 +48,68 @@ unsigned long currentMillis;
 unsigned long previousMillis = 0;
 
 int x, y;
+int smooth = 1;
+int exOuter = 0;
+int exInner = 0;
+int comma1, comma2, comma3, comma4;
 String rawData;
-int last = 0;
 
-// const int FG_PIN = 13; // Interrupt pin for FG signal (digital pin 2 on Arduino UNO)
-// const int pulsesPerRevolution = 6; // 6 FG pulses per revolution
-// unsigned long previousMillis = 0;	// To store the last time calculation was done
-// const unsigned long interval = 1000; // Interval for calculating RPM (1 second)
-// volatile int pulseCount = 0;	// Pulse counter
-// float rpm = 0;				// Motor speed in RPM
-// // Interrupt Service Routine (ISR) to count pulses
-// void countPulses() {
-// 	pulseCount++;
-// }
-// int getRPM(int pin) {
-// 	unsigned long currentMillis = millis();
-// 	int count = 0;
-// 	if (currentMillis - previousMillis >= interval) {
-// 	// Disable interrupts to safely read pulseCount
-// 		noInterrupts();
-// 		count = pulseCount;
-// 		pulseCount = 0;	// Reset the counter
-// 		interrupts();
-// 	}
-// 	previousMillis = currentMillis;
-// 	// Calculate RPM
-// 	return rpm = (count / (float)pulsesPerRevolution) * (60.0 / (interval / 1000.0));
-// }
-
-void getRPM() {
-	Wire.write(String("akbar").c_str());
+void sendRPM() {
+	Wire.write(int(rpm1));
+	Wire.write(int(rpm2));
+	Wire.write(int(rpm3));
+	Wire.write(int(rpm4));
 }
 
 void setup() {
-	// pinMode(FG_PIN, INPUT);
-	// attachInterrupt(digitalPinToInterrupt(FG_PIN), countPulses, RISING);
-	// pinMode(MOTOR1_FG_PIN, INPUT);
-	// pinMode(MOTOR2_FG_PIN, INPUT);
-	// pinMode(MOTOR3_FG_PIN, INPUT);
-	// pinMode(MOTOR4_FG_PIN, INPUT);
-	// Starts I2C as SLAVE 9
-	Wire.begin(I2C_ID);
+	// Starts I2C as SLAVE
+	Wire.begin(slaveID);
+	Wire.setTimeout(3000);
 	Wire.onReceive(getData);
-	Wire.onRequest(getRPM);
+	Wire.onRequest(sendRPM);
 	Serial.begin(9600);
 }
 
 class Motor {
 	public:
-		int mySpeed;
 		int myPin;
 		int myDirPin;
-		Motor(int motPin, int dirPin) {
+		Motor(int motPin, int dirPin, int rpmPin) {
 			myPin = motPin;
 			myDirPin = dirPin;
-			pinMode(myPin, OUTPUT);	pinMode(myDirPin, OUTPUT);
+			pinMode(myPin, OUTPUT);	pinMode(myDirPin, OUTPUT); pinMode(rpmPin, INPUT);
 		}
 
 	void update(int speed) {
-		if (mySpeed > 0) {
+		if (speed > 0) {
 			pinMode(myDirPin, OUTPUT);	// CW Rotation
-			// analogWrite(myDirPin, LOW);		// Try without this line
 		} else {
 			pinMode(myDirPin, INPUT);	// CCW Rotation
 		}
-		analogWrite(myPin, abs(mySpeed));
-	}
-
-	void stop() {
-		update(0);
+		analogWrite(myPin, abs(speed));
 	}
 };
-Motor motors[] = {Motor(mot1, mot1Dir), Motor(mot2, mot2Dir), Motor(mot3, mot3Dir), Motor(mot4, mot4Dir)};
+Motor motors[] = {Motor(mot1, mot1Dir, mot1RPM), Motor(mot2, mot2Dir, mot2RPM), Motor(mot3, mot3Dir, mot3RPM), Motor(mot4, mot4Dir, mot4RPM)};
 
 void updateQuadDrive(int x, int y) {
-	Serial.print("x: "); Serial.print(x); Serial.print("    y: "); Serial.println(y);
+	// Serial.print("x: "); Serial.print(x); Serial.print("	y: "); Serial.println(y);
 	motors[0].update(x);
-	motors[1].update(x);
-	motors[2].update(y);
+	motors[1].update(y);
+	motors[2].update(x);
 	motors[3].update(y);
 }
 
 void smoothValue() {
 // Smooths out engine movement
 	currentMillis = millis();
-	if ((currentMillis - previousMillis >= 1) and (smoothness > 0)) {
+	if ((currentMillis - previousMillis >= 10) and (smoothness > 0)) {
 		hasUpdated = false;
 		if (currentX != x) {
-			currentX = x > currentX ? min(currentX + smoothness / 1000, x) : max(currentX - smoothness / 1000, x);
+			currentX = x > currentX ? min(currentX + smoothness / 100, x) : max(currentX - smoothness / 100, x);
 			hasUpdated = true;
 		}
 		if (currentY != y) {
-			currentY = y > currentY ? min(currentY + smoothness / 1000, y) : max(currentY - smoothness / 1000, y);
+			currentY = y > currentY ? min(currentY + smoothness / 100, y) : max(currentY - smoothness / 100, y);
 			hasUpdated = true;
 		}
 		if (hasUpdated) {updateQuadDrive(currentX, currentY);}
@@ -150,57 +119,101 @@ void smoothValue() {
 }
 
 void getData(int bytes) {
-	while (Wire.available()) {		// Loop through all received bytes
-		char c = Wire.read();		// Receive byte as a character
-		rawData += c;
+	// Loop through all received bytes, receive byte as a character
+	char rawData[32];
+	int index = 0;
+	while (Wire.available() && index < sizeof(rawData) - 1) {
+		rawData[index++] = Wire.read();
 	}
+	rawData[index] = '\0';  // Null-terminate the string
 
-	// Decodes data
-	int comma = rawData.indexOf(',');
-	x = rawData.substring(0, comma).toInt();
-	y = rawData.substring(comma + 1).toInt();
-	rawData = "";
-	if (smoothness <= 0) {
+	// Parse the data
+	char *ptr = strtok(rawData, ",");
+	x = atoi(ptr);
+	ptr = strtok(NULL, ",");
+	y = atoi(ptr);
+	ptr = strtok(NULL, ",");
+	smooth = atoi(ptr);
+	ptr = strtok(NULL, ",");
+	exInner = atoi(ptr);
+	ptr = strtok(NULL, ",");
+	exOuter = atoi(ptr);
+
+	// while (Wire.available()) {
+	// 	char c = Wire.read();
+	// 	rawData += c;
+	// }
+	// // Decodes data
+	// comma1 = 	rawData.indexOf(',');
+	// comma2 = 	rawData.indexOf(',', comma1 + 1);
+	// comma3 = 	rawData.indexOf(',', comma2 + 1);
+	// comma4 = 	rawData.indexOf(',', comma3 + 1);
+	// x = 		rawData.substring(0, comma1).toInt();
+	// y = 		rawData.substring(comma1 + 1).toInt();
+	// smooth = 	rawData.substring(comma2 + 1, comma3).toInt();
+	// exInner = 	rawData.substring(comma3 + 1, comma4).toInt();
+	// exOuter = 	rawData.substring(comma4 + 1).toInt();
+	// rawData = "";
+
+
+	movePiston(true, exInner);
+	movePiston(false, exOuter);
+	if (smooth == 0) {
+		// Instantly stop motors
+		currentX = 0; currentY = 0;
 		updateQuadDrive(x, y);
 	}
 }
 
 
-void loop() {
-	smoothValue();
-	// // Read current state of each FG pin
-	// int currentState1 = digitalRead(MOTOR1_FG_PIN);
-	// int currentState2 = digitalRead(MOTOR2_FG_PIN);
-	// int currentState3 = digitalRead(MOTOR3_FG_PIN);
-	// int currentState4 = digitalRead(MOTOR4_FG_PIN);
-
-	// // Detect rising edge for each motor and count pulses
-	// if (currentState1 == HIGH && lastState1 == LOW) pulseCount1++;
-	// if (currentState2 == HIGH && lastState2 == LOW) pulseCount2++;
-	// if (currentState3 == HIGH && lastState3 == LOW) pulseCount3++;
-	// if (currentState4 == HIGH && lastState4 == LOW) pulseCount4++;
-
-	// // Update last state
-	// lastState1 = currentState1;
-	// lastState2 = currentState2;
-	// lastState3 = currentState3;
-	// lastState4 = currentState4;
-
-	// // Calculate RPM every rpmCalcInterval milliseconds
-	// if (millis() - lastMillis >= rpmCalcInterval) {
-	// 	lastMillis = millis();
-
-	// 	// Calculate RPM
-	// 	rpm1 = (pulseCount1 * 60.0) / (rpmCalcInterval / 1000.0);
-	// 	rpm2 = (pulseCount2 * 60.0) / (rpmCalcInterval / 1000.0);
-	// 	rpm3 = (pulseCount3 * 60.0) / (rpmCalcInterval / 1000.0);
-	// 	rpm4 = (pulseCount4 * 60.0) / (rpmCalcInterval / 1000.0);
-
-	// 	// Reset pulse counts for the next interval
-	// 	pulseCount1 = 0;
-	// 	pulseCount2 = 0;
-	// 	pulseCount3 = 0;
-	// 	pulseCount4 = 0;
-
-	// }
+void movePiston(bool Inner, int val) {
+	Serial.print("wrote "); Serial.print(val > 0 ? "HIGH" : "LOW"); Serial.print(" to "); Serial.println(Inner ? "pistonInnerExt" : "pistonOuterExt");
+	digitalWrite(Inner ? pistonInnerExt : pistonOuterExt,   val > 0 ? HIGH : LOW);
+	digitalWrite(Inner ? pistonInnerRetr : pistonOuterRetr, val < 0 ? HIGH : LOW);
+	if (Inner and val == 0) {
+		digitalWrite(pistonInnerExt, LOW);
+	}
 }
+
+void countRPM() {
+	int currentState1 = digitalRead(mot1RPM);
+	int currentState2 = digitalRead(mot2RPM);
+	int currentState3 = digitalRead(mot3RPM);
+	int currentState4 = digitalRead(mot4RPM);
+
+	// Detect rising edge for each motor and count pulses
+	if (currentState1 == HIGH && lastState1 == LOW) pulseCount1++;
+	if (currentState2 == HIGH && lastState2 == LOW) pulseCount2++;
+	if (currentState3 == HIGH && lastState3 == LOW) pulseCount3++;
+	if (currentState4 == HIGH && lastState4 == LOW) pulseCount4++;
+
+	// Update last state
+	lastState1 = currentState1;
+	lastState2 = currentState2;
+	lastState3 = currentState3;
+	lastState4 = currentState4;
+
+	// Calculate RPM every rpmCalcInterval milliseconds
+	if (millis() - lastMillis >= rpmCalcInterval) {
+		lastMillis = millis();
+
+		// Calculate RPM
+		rpm1 = (pulseCount1 * 60.0) / (rpmCalcInterval * 0.56925);
+		rpm2 = (pulseCount2 * 60.0) / (rpmCalcInterval * 0.56925);
+		rpm3 = (pulseCount3 * 60.0) / (rpmCalcInterval * 0.56925);
+		rpm4 = (pulseCount4 * 60.0) / (rpmCalcInterval * 0.56925);
+		// Reset pulse counts for the next interval
+		pulseCount1 = 0;
+		pulseCount2 = 0;
+		pulseCount3 = 0;
+		pulseCount4 = 0;
+		// Serial.print("mot1: "); Serial.print(rpm1); Serial.print("   mot2: "); Serial.println(rpm2);
+	}
+}
+
+
+void loop() {
+	if (smooth == 1) {smoothValue();}
+	// countRPM();
+}
+
